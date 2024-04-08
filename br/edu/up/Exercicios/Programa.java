09e9b9025ba6a9c574e5956ed46bcf089d9d49cd
viniciustravenssoli <vinicioussouza@gmail.com>
2024-04-08 09:12:13 -0300

    fix

--- br/edu/up/Exercicios/Programa.java
+++ br/edu/up/Exercicios/Programa.java
@@ -3,9 +3,9 @@ package br.edu.up.Exercicios;
 public class Programa {
     public static void main(String[] args) {
 
-        Exercicio01.Executar();
+        // Exercicio01.Executar();
         // Exercicio02.Executar();
-        // Exercicio03.Executar();
+        Exercicio03.Executar();
         // Exercicio04.Executar();
     }
 }
