2444d728037acc873ba10711c5aef4038f9c73b8
makzs <erikmakiak@gmail.com>
2024-04-08 09:34:15 -0300

    Exercicio04

---
+++ br/edu/up/Models/Conversor.java
@@ -0,0 +1,22 @@
+package br.edu.up.Models;
+
+public class Conversor {
+    private double cotacao;
+    private double dollar;
+    
+    public double getCotacao() {
+        return cotacao;
+    }
+    public double getDollar() {
+        return dollar;
+    }
+    
+    public void setCotacao(double cotacao) {
+        this.cotacao = cotacao;
+    }
+    public void setDollar(double dollar) {
+        this.dollar = dollar;
+    }
+
+    
+}
