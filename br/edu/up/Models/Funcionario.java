8a90ba93af88a162710f6033ace49ad4940bf974
makzs <erikmakiak@gmail.com>
2024-04-12 09:28:47 -0300

    exercicio 16

---
+++ br/edu/up/Models/Funcionario.java
@@ -0,0 +1,15 @@
+package br.edu.up.Models;
+
+public class Funcionario {
+    double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    
+}
